977afbe4eddba84573a63bb5345511d309a64d4d
uLuKaiDev <rflaport@gmail.com>
2024-10-17 22:57:08 +0300

    Added player class and draw on main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
 
     # Main game loop starts here"
     
@@ -15,8 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill('black')
-        pygame.display.flip()
+        screen.fill("black")        # Fills the screen black
+        player.draw(screen)         # Draws the player on top of it
+        
+        pygame.display.flip()       # Updates the screen THIS HAS TO BE AT THE BOTTOM
 
         # Limit framerate to 60 (dt = delta time)
         dt = clock.tick(60) / 1000
