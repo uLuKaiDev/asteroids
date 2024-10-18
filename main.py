8cf517a7e53d648350594862a4833b109a582540
uLuKaiDev <rflaport@gmail.com>
2024-10-18 22:11:46 +0300

    Added fwd+bckwd movement and asteroids spawning randomly

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
 
@@ -18,9 +31,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")        # Fills the screen black
-        player.draw(screen)         # Draws the player on top of it
-        player.update(dt)
+        screen.fill("black")        # Fills the screen with black       
+        
+        for obj in updatable:
+            obj.update(dt)     # Updates the rotation and speed
+        
+        for obj in drawable:       # Draws the items on the screen
+            obj.draw(screen)
+        
         pygame.display.flip()       # Updates the screen THIS HAS TO BE AT THE BOTTOM
 
         # Limit framerate to 60 (dt = delta time)
